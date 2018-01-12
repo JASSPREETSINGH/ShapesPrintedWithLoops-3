290a4bb12d975d37a4cb34a0ee6c32a0271820a1
Jaspreet Singh <>
2018-01-12 18:17:09 -0500

    multiplication in tabular form

--- src/timetable/TimeTable.java
+++ src/timetable/TimeTable.java
@@ -15,7 +15,17 @@ public class TimeTable {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+         int size = 9;    // size of the board
+       
+         for (int row = 1; row <= size; ++row) {  // Use row and col, NOT i and j, or x and y
+         
+           for (int col = 1; col<=size; ++col) {
+           int product;
+           product=row*col;
+           System.out.printf("%3d",product);   // print the numbers in appropriate format
+          }
+        System.out.println();     // print a newline
+      } 
     }
     
 }
